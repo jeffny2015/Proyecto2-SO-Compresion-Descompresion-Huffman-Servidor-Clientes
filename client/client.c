b1d7c918f77a73426582efb2545dc0420ccd64bc
jeffny2015 <jeffny2015@gmail.com>
2018-11-16 22:21:16 -0500

    apariciones

--- client/client.c
+++ client/client.c
@@ -22,8 +22,46 @@ int tamanio_archivo;
 char buffer[BUFSIZ];
 int datos_pendientes = 0;
 FILE *archivoAcomprimir;
+
+char *lista_caracter;
+int *lista_apariciones;
+int len_lista;
 //char buff[28];
 
+
+int inicializarLista(){
+    lista_caracter = (char *) malloc(sizeof(char));
+    lista_apariciones = (int *) malloc(sizeof(int));
+    return 0;
+}
+
+int agregarElemento(char caracter){
+    lista_caracter = realloc(lista_caracter,(len_lista+1)*sizeof(char));
+    lista_apariciones = realloc(lista_apariciones,(len_lista+1)*sizeof(int));
+    lista_caracter[len_lista] = caracter;
+    lista_apariciones[len_lista] = 1;
+    len_lista++;
+    return 0;
+}
+
+int estaEnLista(char caracter){
+    for (int i = 0; i < len_lista; i++){
+        if (caracter == lista_caracter[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+int aumentarAparicion(int i){
+    lista_apariciones[i]++;
+    return 0;
+}
+int imprimirLista(){
+    for (int i = 0; i < len_lista; i++){
+        printf("Caracter: %c  => Cantidad: %d\n",lista_caracter[i],lista_apariciones[i]);
+    }
+    return 0;
+}
 int iniciarConfig(const char *nombre){
     FILE *archivo;
     archivo = fopen(nombre,"r");
@@ -98,6 +136,26 @@ void iniciarSocketTCP(char *ip,int puerto){
         datos_pendientes -= len;
         fprintf(stdout, "[-] Se recibieron %ld bytes y se esperaban %d bytes\n", len, datos_pendientes);
     }
+    len_lista = inicializarLista();
+    //int f;
+    FILE *f =fopen(archivoAcomprimir,"r");
+    //f = open(, O_RDONLY);
+    int c;
+    int esta;
+    while(1) {
+      c = fgetc(f);
+      if( feof(f) ) { 
+         break;
+      }
+      esta = estaEnLista((char)c);
+      if (esta != -1){
+        aumentarAparicion(esta);
+      }else{
+        agregarElemento((char)c);
+      }
+      printf("%c", c);
+    }
+    fclose(f);
 
 
         //strcpy(buffer, "hola mundo");
